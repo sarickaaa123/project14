d4f040ca45de21b0869e180aac196133acb27602
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 15:37:49 +0100

    first commit

---
+++ Broevi.java
@@ -0,0 +1,13 @@
+package project_14;
+import java.util.Scanner;
+public class Broevi {
+
+	public static void main(String[] args) {
+	int  sump=0;
+	Scanner  n=new  Scanner(System.in);
+	System.out.println("Vnesi gi broevite:");
+	
+
+	}
+
+}
